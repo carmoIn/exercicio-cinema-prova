4530f695c195c591923af108e19760d997e36f5b
Helcio Laurentino do Carmo Junior <helcio@carmo.in>
2021-10-04 21:32:33 -0300

    feat: Implementa uso na classe Main

--- src/com/helcio/cinema/Main.java
+++ src/com/helcio/cinema/Main.java
@@ -1,8 +1,17 @@
 package com.helcio.cinema;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        ArrayList<Sessao> sessoes = new ArrayList<>();
+        Sessao sessao14 = new Sessao(0L, 0L, 14, 16);
+        sessoes.add(sessao14);
+
+	    Cinema cineCatartas = new Cinema(0L, "Cine Cataratas", 14, 22, sessoes);
+        System.out.println(cineCatartas);
+
+        System.out.println(cineCatartas.getSessoes().get(0));
     }
 }
